8a7608d9e9031468618c0dbf680b63b2e7e94791
Sylvester George <geoslyman57@gmail.com>
2021-07-15 17:31:35 +0100

    upload function

--- public/modules.js
+++ public/modules.js
@@ -1,6 +1,8 @@
 import authenticate from "./authenticate.js";
-import saveInLocalStorage from "./localStorage.js"
+import saveInLocalStorage from "./localStorage.js";
+import handleUpload from "./handleUpload.js";
 
 window.lowdefy.registerJsAction("authenticate", authenticate);
 window.lowdefy.registerJsAction("set", saveInLocalStorage.set);
 window.lowdefy.registerJsAction("get", saveInLocalStorage.get);
+window.lowdefy.registerJsAction("handleUpload", handleUpload);
